125c87f784776999b01d74213f9960039f72a57e
ksuke5110 <keisuke.yamauchi.321@gmail.com>
2019-07-05 23:26:46 +0900

    各種データベース作成

--- database/migrations/2019_07_05_231107_create_team_members_table.php
+++ database/migrations/2019_07_05_231107_create_team_members_table.php
@@ -15,8 +15,8 @@ class CreateTeamMembersTable extends Migration
     {
         Schema::create('team_members', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('team_id');
-            $table->integer('user_id');
+            $table->integer('team_id')->unsigned();
+            $table->integer('user_id')->unsigned();
             $table->boolean('admin');
             $table->timestamps();
             //外部キー

c3504380854f92588da686ec1aff5db85276fb63
ksuke5110 <keisuke.yamauchi.321@gmail.com>
2019-08-19 18:00:28 +0900

    tests実装

--- tests/Feature/AccessTest.php
+++ tests/Feature/AccessTest.php
@@ -12,8 +12,36 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class AccessTest extends TestCase
 {
+
+    use RefreshDatabase;
+
+    public function testAllUserAccess()
+    {
+        // 未ログインユーザーもアクセス可能
+        $responce = $this->get('/');
+        $responce->assertStatus(200);
+
+        $responce = $this->get('/search');
+        $responce->assertStatus(200);
+
+        $response = $this->get('/users/create');
+        $response->assertStatus(200);
+
+        $responce = $this->from('/users/create')->post('/register', ['name' => '山田 太郎', 'email' => 'example@example.com', 'password' => 'password']);
+        $this->assertDatabaseHas('users', ['name' => '山田 太郎']);
+        $responce->assertStatus(200);
+    }
+
     public function testAdminAccess()
     {
+
+        //管理者のみアクセス可能
+        $responce = $this->get('/users');
+        $responce->assertStatus(302);
+
+        $response = $this->get('/admin');
+        $response->assertStatus(302);
+
         //外部キー制限解除
         Schema::disableForeignKeyConstraints();
 
@@ -22,7 +50,6 @@ class AccessTest extends TestCase
         ]);
 
         $response = $this->actingAs($user)->get('/admin');
-        dump($response);
         $response->assertStatus(200);
 
         $user = factory(User::class)->create([

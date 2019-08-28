defdf9040fe74b84c06b4ed617692056002f94cf
ksuke5110 <keisuke.yamauchi.321@gmail.com>
2019-08-28 21:35:57 +0900

    PostControllerテスト作成

--- tests/Unit/PostControllerTest.php
+++ tests/Unit/PostControllerTest.php
@@ -3,6 +3,8 @@
 namespace Tests\Unit;
 
 use App\Models\Post;
+use App\Models\Team;
+use App\Models\User;
 use Illuminate\Support\Facades\Schema;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
@@ -10,7 +12,6 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class PostControllerTest extends TestCase
 {
-
     use RefreshDatabase;
 
     /**
@@ -21,24 +22,61 @@ class PostControllerTest extends TestCase
         //外部キー制限解除
         Schema::disableForeignKeyConstraints();
 
-        // ダミーで利用するデータ
+        // 投稿ユーザー作成
+        factory(User::class)->create();
+        $user = User::find(1);
+        $data = $user->toArray();
+        $this->assertDatabaseHas('users', $data);
+
+        // 投稿先チーム作成
+        factory(Team::class)->create([
+            'user_id' => 1,
+        ]);
+        $team = Team::find(1);
+        $data = $team->toArray();
+        $this->assertDatabaseHas('teams', $data);
+
+
+        // 活動状況一覧 index
+        $response = $this->get('posts?team_id=' . $team->id);
+        $response->assertOk();
+
+        // 活動状況投稿画面 create
+        $response = $this->actingAs($user)->get('posts/create');
+        $response->assertOk();
+
+        // 活動状況投稿 store
         $data = [
+            'id' => 1,
             'title'    => 'タイトルタイトル',
             'body'       => 'ボディボディボディボディ',
+            'user_id' => $user->id,
+            'team_id' => $team->id,
         ];
-        $post = factory(Post::class)->create($data);
+        $this->actingAs($user)->post('posts', $data);
+        $this->assertDatabaseHas('posts', $data);
+        $post = Post::find(1);
 
-        //ダミーデータを取得
+        // 活動状況詳細画面 show
+        $response = $this->actingAs($user)->get('posts/' . $post->id);
+        $response->assertOk();
+
+        // 活動状況編集画面 edit
+        $response = $this->actingAs($user)->get('posts/' . $post->id . '/edit');
+        $response->assertOk();
+
+        // 活動状況更新 update
+        $data = [
+            'user_id' => $user->id,
+            'team_id' => $team->id,
+            'title'    => 'タタタタタイトル',
+            'body'       => 'ボボボボディ',
+        ];
+        $this->actingAs($user)->put('posts/' . $post->id, $data);
         $this->assertDatabaseHas('posts', $data);
 
-        //ダミーデータを更新
-        $post->title = 'タタタタタイトル';
-        $post->save();
-        $data['title'] = 'タタタタタイトル';
-        $this->assertDatabaseHas('posts', $data);
-
-        //ダミーデータを削除
-        $post->delete();
+        // 活動状況削除 destroy
+        $this->actingAs($user)->delete('posts/' . $post->id);
         $this->assertDatabaseMissing('posts', $data);
 
         //外部キー制限解除を解除
